dcfa235646341eef67e122eaf071405ee381efe0
Michał Woźniak <m.wozniak@ideo.pl>
2022-12-27 20:44:03 +0100

    Add project files.

---
+++ Prodex/Client/NodeJs/src/index.js
@@ -0,0 +1,30 @@
+﻿import BpmnModeler from 'bpmn-js/lib/Modeler';
+
+window.loadBpmn = () => {
+
+    var canv = document.querySelector('#bpmn-canvas');
+
+    if (canv.hasChildNodes())
+        return;
+
+    window.modeler = new BpmnModeler({
+        container: '#bpmn-canvas'
+    });
+
+
+    try {
+        window.modeler.createDiagram();
+
+        console.log('rendered');
+    } catch (err) {
+        console.log('error rendering', err);
+    }
+}
+
+window.getCurrentDiagram = async () => {
+    if (!window.modeler) return null;
+
+    return (await window.modeler.saveXML({ format: true })).xml;
+}
+
+
