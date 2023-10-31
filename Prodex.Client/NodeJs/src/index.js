ec531c0598c13e95fe9b933a798999fe3f17648a
Michał Woźniak <m.wozniak@ideo.pl>
2023-10-31 11:45:10 +0100

    #23 - [Modelowanie procesu] kolorowanie komponentów

--- Prodex.Client/NodeJs/src/index.js
+++ Prodex.Client/NodeJs/src/index.js
@@ -34,6 +34,23 @@ window.setCurrentDiagram = async (xml) => {
     if (!window.modeler) return;
 
     await window.modeler.importXML(xml);
+
+    const elementRegistry = window.modeler.get("elementRegistry").getAll();
+    const modeling = window.modeler.get("modeling");
+
+    const colorsMap = [
+        { type: "bpmn:StartEvent",       color: "#2eb85c" },
+        { type: "bpmn:UserTask",         color: "#52BAE8" },
+        { type: "bpmn:ServiceTask",      color: "#E4F6FF" },
+        { type: "bpmn:SendTask",         color: "#FFF3C3" },
+        { type: "bpmn:ExclusiveGateway", color: "#FFBA52" },
+        { type: "bpmn:EndEvent",         color: "red"  },
+    ]
+
+    for (let item of colorsMap) {
+        const elements = elementRegistry.filter(p => p.type == item.type);
+        modeling.setColor(elements, { stroke: '#222222', fill: item.color })
+    }
 }
 
 
